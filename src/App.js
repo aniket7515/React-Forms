2543ae2e23d09bd252bc390fb58990b9d9f04882
Aniket <aniketdhokane2071@gmail.com>
2022-01-19 17:08:42 +0530

    Login Form submmit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,46 @@ import './App.css';
 
 function App() {
   const [ name, setName]=useState("");
+  const [ password, setPassword]=useState("");
   const [ fullName, setFullName]=useState("");
+  const [ fullPassword, setFullPassword]=useState("");
 
   const inputEvent = (event) =>{
     // console.log(event.target.value);
     setName(event.target.value);
+    
+  }
+  const inputEvent1 = (event) =>{
+    // console.log(event.target.value);
+   
+    setPassword(event.target.value);
   }
 
-  const onSubmit = () =>{
+  const onSubmit = (event) =>{
+    event.preventDefault(); // use that form should not use its default behaviour
     setFullName(name);
+    setFullPassword(password);
   }
 
 
   return (
     <div className="App">
-      <h1>Hello {fullName}</h1>
-      <input type="text" placeholder="Enter Your Name :" onChange={inputEvent} value={name}/>
-      <button onClick={onSubmit}>Click Me</button>
+      <form onSubmit={onSubmit}>
+         <h1>Hello {fullName}{fullPassword}</h1>
+         <div>
+         <input type="text" placeholder="Enter Your Name :" onChange={inputEvent} value={name}/>
+
+         </div>
+         <div>
+         <input type="text" placeholder="Enter Your Password :" onChange={inputEvent1} value={password}/>
+
+         </div>
+        
+         
+         <button  type="submit">Click Me</button>
+
+      </form>
+     
       
     </div>
   );
