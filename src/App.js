932d24fa8fad9234ecefd24636a29db94064695f
Aniket <aniketdhokane2071@gmail.com>
2022-01-19 16:47:14 +0530

    forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [ name, setName]=useState("");
+  const [ fullName, setFullName]=useState("");
+
+  const inputEvent = (event) =>{
+    // console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  const onSubmit = () =>{
+    setFullName(name);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {fullName}</h1>
+      <input type="text" placeholder="Enter Your Name :" onChange={inputEvent} value={name}/>
+      <button onClick={onSubmit}>Click Me</button>
+      
     </div>
   );
 }
